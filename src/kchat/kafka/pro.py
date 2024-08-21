9e847468cd915859d9297a42339e4199c7aa1654
DONGUK777 <kimdongyork@gmail.com>
2024-08-21 16:48:08 +0900

    My OFFSET

--- src/kchat/kafka/pro.py
+++ src/kchat/kafka/pro.py
@@ -4,15 +4,15 @@ import json
 from tqdm import tqdm
 
 pro = KafkaProducer(
-        bootstrap_servers=['localhost:9092'],
-        value_serializer=lambda x:json.dumps(x).encode('utf-8')
-        )
+    bootstrap_servers=['localhost:9092'],
+    value_serializer=lambda x:json.dumps(x).encode('utf-8')
+)
 
 start = time.time()
 
 for i in tqdm(range(10)):
     data = {'str': 'value' + str(i)}
-    pro.send('topic1', value=data)
+    pro.send('topic2', value=data)
     pro.flush()
     time.sleep(0.1)
 
